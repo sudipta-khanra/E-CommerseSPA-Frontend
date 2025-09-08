01193b7557f42bbf156aaa0a2643bf94feb2057a
sudipta khanra <sudiptakhanra0612@gmail.com>
2025-09-08 11:10:05 +0530

    Bug Fix

--- src/api.js
+++ src/api.js
@@ -8,7 +8,7 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
-    config.headers["Authorization"] = `Bearer ${token}`; // Standard JWT header
+    config.headers["x-auth-token"] = token; // ✅ backend expects x-auth-token
   }
   return config;
 });
